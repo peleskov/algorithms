9061e672c9ff117aced2538cce52e4b72feda393
peleskov <peleskov@yandex.ru>
2022-04-27 09:28:15 +0300

    ex_3 fix

--- lesson_3/lesson_3_ex_3.py
+++ lesson_3/lesson_3_ex_3.py
@@ -7,7 +7,7 @@ RANGE_MIN = -100
 RANGE_MAX = 100
 RANGE_LEN = 10
 
-range_ = [random.randint(RANGE_MIN, RANGE_MAX) for _ in range(0, RANGE_LEN + 1)]
+range_ = [random.randint(RANGE_MIN, RANGE_MAX) for _ in range(0, RANGE_LEN)]
 print(range_)
 
 max_ = [0, range_[0]]  # index, value
