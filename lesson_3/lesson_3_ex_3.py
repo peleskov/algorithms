430290d088bae067c6398dbc2627c3ccda80a5d3
peleskov <peleskov@yandex.ru>
2022-04-27 08:00:53 +0300

    ex_3

---
+++ lesson_3/lesson_3_ex_3.py
@@ -0,0 +1,23 @@
+"""
+    В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
+"""
+import random
+
+RANGE_MIN = -100
+RANGE_MAX = 100
+RANGE_LEN = 10
+
+range_ = [random.randint(RANGE_MIN, RANGE_MAX) for _ in range(0, RANGE_LEN + 1)]
+print(range_)
+
+max_ = [0, range_[0]]  # index, value
+min_ = [0, range_[0]]
+for n, i in enumerate(range_):
+    if i < min_[1]:
+        min_ = [n, i]
+    elif i > max_[1]:
+        max_ = [n, i]
+
+range_[min_[0]],  range_[max_[0]] = range_[max_[0]], range_[min_[0]]
+print(range_)
+print(min_, max_)
