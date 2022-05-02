3209966eedb0a03ddc23f533edd12ec17d9d1a6c
peleskov <peleskov@yandex.ru>
2022-05-02 09:22:29 +0300

    ex_3 fix

--- lesson_3/lesson_3_ex_3.py
+++ lesson_3/lesson_3_ex_3.py
@@ -10,14 +10,15 @@ RANGE_LEN = 10
 range_ = [random.randint(RANGE_MIN, RANGE_MAX) for _ in range(0, RANGE_LEN)]
 print(range_)
 
-max_ = [0, range_[0]]  # index, value
-min_ = [0, range_[0]]
+max_index, max_val = 0, range_[0]
+min_index, min_val = 0, range_[0]
 for n, i in enumerate(range_):
-    if i < min_[1]:
-        min_ = [n, i]
-    elif i > max_[1]:
-        max_ = [n, i]
+    if i < min_val:
+        min_index, min_val = n, i
+    elif i > max_val:
+        max_index, max_val = n, i
 
-range_[min_[0]],  range_[max_[0]] = range_[max_[0]], range_[min_[0]]
+range_[min_index],  range_[max_index] = range_[max_index], range_[min_index]
 print(range_)
-print(min_, max_)
+print(f'{min_index=}', f'{min_val=}')
+print(f'{max_index=}', f'{max_val=}')
