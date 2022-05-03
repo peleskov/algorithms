69e03e00862c6d95078a4e43975b66c216b0d7b6
peleskov <peleskov@yandex.ru>
2022-05-03 10:48:52 +0300

    ex_7 fix

--- lesson_3/lesson_3_ex_7.py
+++ lesson_3/lesson_3_ex_7.py
@@ -11,12 +11,24 @@ RANGE_LEN = 10
 range_ = [random.randint(RANGE_MIN, RANGE_MAX) for _ in range(0, RANGE_LEN)]
 print(range_)
 
-res = [None, None]
-for i in range_:
-    if res[0] is None or i < res[0]:
-        res[1], res[0] = res[0], i
-    elif res[1] is None or i == res[0] or i < res[1]:
-        res[1] = i
 
-print(f'Два наименьших элемента: {res}')
+def min_():
+    min_1 = None
+    min_2 = None
+    for i in range_:
+        if min_1 is None:
+            min_1 = i
+        elif i < min_1:
+            min_2, min_1 = min_1, i
+        elif i > min_1:
+            if min_2 is None:
+                min_2 = i
+            elif i < min_2:
+                min_2 = i
+    if min_2 is None:
+        min_2 = min_1
+    return min_1, min_2
+
+
+print(f'Два наименьших элемента: {min_()}')
 
